2f7c572d582bfa57e23a747753298d3f1701efba
Ooi Ming Sheng <ooimingsheng@gmail.com>
2019-05-10 13:16:47 +0000

    implement advance login

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,5 @@
 class User < ApplicationRecord
-  
+  attr_accessor :remember_token
   
   has_secure_password
   
@@ -19,5 +19,27 @@ class User < ApplicationRecord
     cost = ActiveModel::SecurePassword.min_cost ? BCrypt::Engine::MIN_COST
                                                 : BCrypt::Engine.cost
     BCrypt::Password.create(string, cost: cost)
+  end
+  
+  # Returns a random token
+  def User.new_token
+    SecureRandom.urlsafe_base64
+  end
+  
+  # Creates a new remember token for the user and stores the remember digest
+  def remember
+    self.remember_token = User.new_token
+    update_attribute(:remember_digest, User.digest(remember_token))
+  end
+  
+  # Returns true if the given token matches the digest
+  def authenticated?(remember_token)
+    return false if remember_digest.nil?
+    BCrypt::Password.new(remember_digest).is_password?(remember_token)
+  end
+  
+  # Forgests a user
+  def forget
+    update_attribute(:remember_digest, nil)
   end
 end
